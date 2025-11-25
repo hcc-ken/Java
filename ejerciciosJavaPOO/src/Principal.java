ba7539f57b81f438868c18477e96ddb38a1b4fcb
hcc <109288656+hcc-ken@users.noreply.github.com>
2025-11-25 11:49:44 +0100

    Add files via upload

---
+++ ejerciciosJavaPOO/src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main (String[] args){
+        Punto p1 = new Punto(5, 0);
+        Punto p2 = new Punto(10, 10);
+        Punto p3 = new Punto(-3,7);
+    }
+}
---
+++ ejerciciosJavaPOO/src/Punto.java
@@ -0,0 +1,21 @@
+public class Punto {
+    private int x, y;
+
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getx() {
+        return x;
+    }
+
+    public int gety() {
+        return y;
+    }
+
+    public void setx() {
+        return this.x
+    }
+
+}
