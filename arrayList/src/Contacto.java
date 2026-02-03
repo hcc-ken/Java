ef876de83d53ff9428acca4fe414c636d312ecce
Huigen Chen Chen <huigenchen1@gmail.com>
2026-02-03 10:59:13 +0100

    Merge de ejercicios

---
+++ arrayList/src/Contacto.java
@@ -0,0 +1,35 @@
+public class Contacto {
+    private String nombre;
+    private String apellidos;
+    private int telefono;
+
+    public Contacto(String nombre, String apellidos, int telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
