940529517ff3eb8b605490ea4d0aa753833c72fd
Huigen Chen Chen <huigenchen1@gmail.com>
2026-02-03 12:07:56 +0100

    ejercicio String

--- arrayString/src/Main.java
+++ arrayString/src/Main.java
@@ -1,37 +1,41 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca su numero de telefono con prefijo.");
-        String phoneNumber = sc.nextLine();
-
-        String [] phoneNumberParts = phoneNumber.split(" ");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Introduzca su numero de telefono con prefijo.");
+//        String phoneNumber = sc.nextLine();
+//
+//        String [] phoneNumberParts = phoneNumber.split(" ");
 //          // Version A
 //        System.out.println("Escriba una frase.");
-//        String greeting = sc.nextLine();
+//        String palabra = sc.nextLine();
 //
-//        String [] greetings = greeting.split(" ");
+//        String [] greetings = palabra.split(" ");
 //        System.out.println(greetings.length);
 //
 //        for (int i = 0; i < greetings.length; i++) {
 //            if (greetings[i] != " ") {
 //                System.out.print(greetings[i] + " ");
 //            }
-//
 //        }
 //
 //         // Version B
-//        System.out.println("Escriba otra frase.");
-//        String greeting = sc.nextLine();
-//
-//        int contador = 1;
-//        for (int i = 0; i < greeting.length(); i++) {
-//            if (greeting.charAt(i) == ' ') {
-//                contador++;
-//            }
-//        }
-//        System.out.println(contador);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba otra frase.");
+        String cadena = sc.nextLine();
+        String palabra = "";
+
+        int contador = 0;
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == ' ') {
+                contador++;
+                System.out.println(palabra);
+                palabra = "";
+            } else {
+                palabra = palabra + cadena.charAt(i);
+            }
+        }
+        System.out.println(palabra);
     }
 }
